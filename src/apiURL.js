5c38c9b0c5417d2113b545980055406eabc48dad
MD Samiul Islam <isamiul099@gmail.com>
2023-12-25 12:48:45 +0530

    Make it better

--- src/apiURL.js
+++ src/apiURL.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-const apiURL = 'http://localhost:5500'
-=======
 const apiURL = 'https://ziventa-server.onrender.com'
->>>>>>> 0065ee6d206b94d843229b3c1195d69757a76dc0
 export default apiURL;
